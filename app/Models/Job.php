fc72e5d92189d04dd7903bd9fe4de1920d9a4a9c
Đào Anh Thành <daoanhthanh.work@gmail.com>
2025-02-16 13:15:35 +0700

    ok

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,52 +2,14 @@
 
 namespace App\Models;
 
-// use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-
-use Illuminate\Support\Arr;
-
-class Job
+class Job extends Model
 {
-    public $id;
-    public $title;
-    public $salary;
+    use HasFactory;
 
-    public function __construct($id, $title, $salary)
-    {
-        $this->id = $id;
-        $this->title = $title;
-        $this->salary = $salary;
-    }
+    protected $table = 'job_listings';
 
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'PHP Developers',
-                'salary' => '$1000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Python Developers',
-                'salary' => '$2000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Java Developers',
-                'salary' => '$3000'
-            ]
-        ];
-    }
-
-
-    public static function find(int $id): array
-    {
-        $jobs = self::all();
-        $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
-
-        return $job;
-        // return new Job($job['id'], $job['title'], $job['salary']);
-    }
+    protected  $fillable = ['title', 'salary'];
 }
